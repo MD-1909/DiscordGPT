5acb40fcd400a8029a9d73b26769d0e7d2898840
MD <mohammeddhange@gmail.com>
2023-03-10 13:53:29 +0530

    Improved code readability

--- index.js
+++ index.js
@@ -12,17 +12,17 @@ const openai = new OpenAIApi(configuration);
 const token = process.env.DISCORD_TOKEN;
 
 //Async function to query a question to the openAI model
-async function ask(prompt) {
+async function sendQuery(prompt) {
   const response = await openai.createCompletion({
-    model: "text-davinci-003", // The model version
-    prompt, // The input
+    model: "text-davinci-003", // The model version of the AI
+    prompt, // The input query that is sent
     temperature: 0.9, // Value between 0 and 2 that influences the output
     // Lower means more deterministic, while higher means more random
     max_tokens: 256, // Max number of tokens (pieces of words)
     frequency_penalty: 0, // Number between -2 and +2
     // Positive values reduce the likelihood of the model using the same verbatim phrases
     presence_penalty: 0, // Number between -2 and +2
-    // Positive values incerase the likelihood of the model talking about new topics
+    // Positive values increase the likelihood of the model talking about new topics
   });
   const answer = response.data.choices[0].text; //Pick out the text from the first response
   return answer;
@@ -46,7 +46,7 @@ client.on(Events.MessageCreate, async (message) => {
   if (message.content.substring(0, 1) === ".") {
     // When a message starts with a '.'
     const prompt = message.content.substring(1); // Remove the '.' from the message
-    const answer = await ask(prompt); // Query the AI
+    const answer = await sendQuery(prompt); // Query the AI
     client.channels
       .fetch(message.channelId)
       .then((channel) => channel.send(answer));
